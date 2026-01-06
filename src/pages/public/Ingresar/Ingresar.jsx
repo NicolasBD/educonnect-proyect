c849e652ddb043c2bdcdd62852cc402de36fe296
NicolasBD <nicofilnicolas9@gmail.com>
2026-01-05 20:32:14 -0500

    Pagina LoingIn / Ingresar

---
+++ src/pages/public/Ingresar/Ingresar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Ingresar.css'
+
+export default function Ingreso() {
+  return (
+    <body class="auth-body">
+
+    <div class="auth-container">
+        <h2>Iniciar Sesión</h2>
+
+        <form id="loginForm">
+            <label>Email</label>
+            <input type="email" id="email" required/>
+
+            <label>Contraseña</label>
+            <input type="password" id="password" required/>
+
+            <button type="submit">Entrar</button>
+        </form>
+
+        <p>¿No tienes cuenta? <a href="registro.html">Regístrate aquí</a></p>
+    </div>
+
+</body>
+  )
+}
